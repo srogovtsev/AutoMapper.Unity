4a0d2644fdcb1cab44bd1c0457ba690d9ed51029
Serg Rogovtsev <lair@hawke.ru>
2014-11-01 13:32:01 +0300

    Added basic implementation

---
+++ Sources/AutoMapper.Unity/ContainerMapperExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using AutoMapper.Mappers;
+
+using Microsoft.Practices.Unity;
+
+namespace AutoMapper.Unity
+{
+	public static class ContainerMapperExtensions
+	{
+		public static void RegisterMappingProfile<T>(this IUnityContainer container)
+			where T : Profile
+		{
+			container.RegisterType<Profile,T>(typeof(T).FullName, new ContainerControlledLifetimeManager());
+		}
+
+		public static void RegisterMapper(this IUnityContainer container)
+		{
+			container.RegisterType<IConfigurationProvider>(new ContainerControlledLifetimeManager(), new InjectionFactory(c =>
+			{
+				var configuration = new ConfigurationStore(new TypeMapFactory(), MapperRegistry.AllMappers());
+				configuration.ConstructServicesUsing(t => container.Resolve(t));
+				foreach (var profile in c.ResolveAll<Profile>())
+					configuration.AddProfile(profile);	
+				return configuration;
+			}
+			));
+			container.RegisterType<IMappingEngine, MappingEngine>(new ContainerControlledLifetimeManager(), new InjectionConstructor(typeof(IConfigurationProvider)));
+		}
+	}
+}
