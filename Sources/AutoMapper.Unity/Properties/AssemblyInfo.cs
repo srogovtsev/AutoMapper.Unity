9865406076d96eb3f429a1c9c88778df4b2c440d
Serg Rogovtsev <lair@hawke.ru>
2014-11-02 12:34:04 +0300

    Moved versioning to build-time

--- Sources/AutoMapper.Unity/Properties/AssemblyInfo.cs
+++ Sources/AutoMapper.Unity/Properties/AssemblyInfo.cs
@@ -21,6 +21,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3774deac-cb9d-460c-8059-24c1544ceba0")]
 
+#region All assembly information will be set at AppVeyor
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -28,9 +29,11 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
+#endregion
+
